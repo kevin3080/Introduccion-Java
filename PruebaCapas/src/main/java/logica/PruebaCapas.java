0a4bbf8517f33d91f99488470901f84d16ff60f4
Kevin Pernia <kevinpernia29@gmail.com>
2023-10-16 14:02:06 -0500

    integracion GUI, final curso

---
+++ PruebaCapas/src/main/java/logica/PruebaCapas.java
@@ -0,0 +1,13 @@
+package logica;
+
+import gui.Pantalla;
+
+public class PruebaCapas {
+
+    public static void main(String[] args) {
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+    
+}
